bf3f080711a7b7755e9f84588cc7b06773017a4b
Алекс Мозаидзе <alexman2622@gmail.com>
2021-12-28 19:16:53 +0400

    Testing new things

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@
     clippy::string_to_string,
     clippy::undocumented_unsafe_blocks,
     clippy::unnecessary_self_imports,
+    clippy::unseparated_literal_suffix,
 
     // nursery lints
     clippy::debug_assert_with_mut_call,
@@ -29,6 +30,7 @@
     clippy::use_self,
 
     // pedantic lints
+    clippy::cloned_instead_of_copied,
     clippy::filter_map_next,
     clippy::if_not_else,
     clippy::from_iter_instead_of_collect,
@@ -63,24 +65,79 @@
 )]
 
 use macroquad::prelude::*;
+use macroquad::ui::{*, widgets::*};
 use egui_macroquad::egui;
+use enumflags2::{bitflags, BitFlags};
+use std::collections::HashMap;
+use std::path::PathBuf;
 //use tinyfiledialogs::{open_file_dialog, color_chooser_dialog, DefaultColorValue};
 //use include_dir::{include_dir, Dir};
-//use enumflags2::{bitflags, make_bitflags, BitFlags};
 
-#[macroquad::main("BasicShapes")]
+const EXAMPLE_TOML_OUTPUT: &str = r#"
+[[entities]]
+name = "pinik"
+x = 0
+y = 0
+w = 26
+h = 26
+
+[[entities]]
+name = "bad_guy"
+x = 200
+y = 200
+w = 120
+h = 140
+"#;
+
+type Map<K, V> = HashMap<K, V>;
+
+#[bitflags]
+#[repr(u8)]
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum Settings {
+    Forget, // Delete old settings of the app and don't save it as long as this option is set
+    Reload, // Reload all the previous images and place them like the last time
+}
+
+#[macroquad::main("Jubilant")]
 async fn main() {
-    // File chooser
-    //println!("{:?}", open_file_dialog("A cool-ass title", "", None));
-    // Color picker
-    //println!("{:?}", color_chooser_dialog(">:D", DefaultColorValue::Hex("#FF00FF")));
-    // Mmmmm, binary-included directory!
-    //println!("{:?}", DATA_DIR.get_file("pussy.txt").unwrap()); // 
+    let settings: BitFlags<Settings> = BitFlags::empty(); // TODO: Load from settings.toml
+
+    let mut text_output = EXAMPLE_TOML_OUTPUT.to_owned();
+
+    let textures: Map<String, Texture2D> = Map::new();
 
     loop {
-        clear_background(GRAY);
+        clear_background(WHITE);
 
-        egui_macroquad::ui(|ctx| { });
+        // Canvas
+        for texture in textures.values().copied() {
+            draw_texture(texture, 0.0, 0.0, WHITE);
+        }
+
+        // GUI
+        egui_macroquad::ui(|ctx| {
+            // Output Window
+            egui::Window::new("Output")
+                .min_width(200_f32)
+                .vscroll(true)
+                .show(ctx, |ui| {
+                    if ui.button("Copy").clicked() {
+                        todo!();
+                    }
+                    if ui.button("Generate").clicked() {
+                        todo!();
+                    }
+                    ui.text_edit_multiline(&mut text_output.as_str());
+                });
+            // Input Window
+            egui::Window::new("Input")
+                .min_width(200_f32)
+                .vscroll(true)
+                .show(ctx, |ui| {
+
+                });
+        });
 
         egui_macroquad::draw();
 
